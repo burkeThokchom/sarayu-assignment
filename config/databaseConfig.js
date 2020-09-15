154e485247f97066597207559b9391a28ebfb108
Burke <burke8746@gmail.com>
2020-09-16 00:55:17 +0530

    config mongo

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -4,9 +4,10 @@ let mongoUrl = `mongodb://${appConfig.DATABASE_HOST}:${appConfig.DATABASE_PORT}/
 
 // for mongodb atlas
 if(appConfig.DATABASE_USER && appConfig.DATABASE_PASS){
-	mongoUrl = `mongodb+srv://${appConfig.DATABASE_USER}:${appConfig.DATABASE_PASS}@furnitureapp.uozjb.mongodb.net/${appConfig.DATABASE_NAME}?retryWrites=true&w=majority`
-	//mongoUrl = `mongodb+srv://${appConfig.DATABASE_USER}:${appConfig.DATABASE_PASS}@${appConfig.DATABASE_HOST}:${appConfig.DATABASE_PORT}/${appConfig.DATABASE_NAME}`
+	mongoUrl = `mongodb+srv://${appConfig.DATABASE_USER}:${appConfig.DATABASE_PASS}@assignmentsdb.v78sh.mongodb.net/${appConfig.DATABASE_NAME}?retryWrites=true&w=majority`
 }
+
+console.log("Printing url ", mongoUrl);
 module.exports = {
 	dbConnectionString: mongoUrl
 }
