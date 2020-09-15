e96a0a8eba53e09924668a1c6687c5243c216f89
Burke <burke8746@gmail.com>
2020-09-16 01:03:37 +0530

    config db commit

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -6,8 +6,6 @@ let mongoUrl = `mongodb://${appConfig.DATABASE_HOST}:${appConfig.DATABASE_PORT}/
 if(appConfig.DATABASE_USER && appConfig.DATABASE_PASS){
 	mongoUrl = `mongodb+srv://${appConfig.DATABASE_USER}:${appConfig.DATABASE_PASS}@assignmentsdb.v78sh.mongodb.net/${appConfig.DATABASE_NAME}?retryWrites=true&w=majority`
 }
-
-console.log("Printing url ", mongoUrl);
 module.exports = {
 	dbConnectionString: mongoUrl
 }
